69ac03ad805378d5f8d1b9af8fc4756c6aa12a03
Richie Haynes <77641979+rhaynes8@users.noreply.github.com>
2021-01-25 22:37:38 +0000

    Update Distortion.cpp

--- Source/Distortion.cpp
+++ Source/Distortion.cpp
@@ -11,74 +11,45 @@
 #include "Distortion.h"
 #include <JuceHeader.h>
 
+
 Distortion::Distortion()
 {
     auto& waveshaper = processorChain.template get<waveshaperIndex>();
     waveshaper.functionToUse = [&, this] (float x)
 
     {
-        //return (std::tanh(pow(10, this->getGain() / 20) * x))/std::tanh(pow(10, this->getGain() / 20));
-        
-    
-        
-        //return tanh(x);
-        
         return tanh(pow(10, this->getGain() / 20) * x);
+ 
         
-        //return juce::jlimit (float (-0.95), float (0.95), x);
-
     };
     
-    mOversampling.reset(new juce::dsp::Oversampling<float>(1, 3, juce::dsp::Oversampling<float>::filterHalfBandFIREquiripple, false));
-
     auto& preGain = processorChain.template get<preGainIndex>();
-    preGain.setGainDecibels (30.0f);
+    preGain.setGainDecibels (20.0f);
     
     auto& postGain = processorChain.template get<postGainIndex>();
-    postGain.setGainDecibels (-20.0f);
+    postGain.setGainDecibels (0.0f);
 }
 
 //==============================================================================
 void Distortion::prepare (const juce::dsp::ProcessSpec& spec)
 {
     auto& filter = processorChain.template get<filterIndex>();
-    //filter.state = FilterCoefs::makeFirstOrderLowPass (spec.sampleRate, 3500.0f);
-    filter.state = FilterCoefs::makeFirstOrderHighPass(spec.sampleRate, 10.0f);
+    filter.state = FilterCoefs::makeFirstOrderLowPass (spec.sampleRate, 3500.0f);
+    filter.state = FilterCoefs::makeFirstOrderHighPass(spec.sampleRate, 50.0f);
+    
     processorChain.prepare (spec);
-    
-    mMaxBlockSize = spec.maximumBlockSize;
-    
-    mOversampling->initProcessing(static_cast<size_t>(mMaxBlockSize));
-
-    reset();
 }
 
 //==============================================================================
 //template <typename ProcessContext>
 void Distortion::process (juce::dsp::ProcessContextReplacing<float> context) noexcept
 {
-    juce::ScopedNoDenormals noDenormals;
-    // Create a block for oversampled, upsampled, data
-    juce::dsp::AudioBlock<float> oversampledBlock = mOversampling -> processSamplesUp(context.getInputBlock());
-    // Create context variable for upsampled data block
-    auto waveshaperContext = juce::dsp::ProcessContextReplacing<float>(oversampledBlock);
-    // Process oversampled block's context
-    processorChain.template get<waveshaperIndex>().process(waveshaperContext);
-    // Scale new incoming data blocks
-    waveshaperContext.getOutputBlock() *= 0.7;
-    
-    
-    // Downsample to original context
-    mOversampling->processSamplesDown(context.getOutputBlock());
-    // Process the distortion block
     processorChain.process (context);
-    
 }
 
 //==============================================================================
 void Distortion::reset() noexcept
 {
-    mOversampling->reset();
     processorChain.reset();
 }
 
@@ -91,3 +62,4 @@ float Distortion::getGain()
 {
     return gain;
 }
+
